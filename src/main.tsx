3a6a89bcb689a322e922233a9252ceb83a90ab39
Lais Ponte Costa <laisdaguerra@gmail.com>
2025-08-17 00:40:13 -0300

    Primeiros passos para editar a timeline

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 import twitterlogo from './assets/Logo.png'
-import {Bell, BookmarkSimple, DotsThreeCircle, Envelope, Hash, House, List, User} from 'phosphor-react'
+import {Bell, BookmarkSimple, DotsThreeCircle, Envelope, Hash, House, List, Sparkle, User} from 'phosphor-react'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='layout'>
@@ -33,7 +33,18 @@ createRoot(document.getElementById('root')!).render(
       </aside>
      
       <div className="content">
-        Teste para conteiner
+        <main className="timeline">
+          <div className="title">Home
+            <Sparkle/>  
+          </div>  
+          <form className='new-tweet'>
+            <label htmlFor="tweet">
+              <img src="https://github.com/Lais-coder.png" alt="Foto Usúario" />
+              <textarea id='tweet' placeholder="What's happigin"/>
+            </label>
+            <button type='submit'>Tweet</button>
+          </form>
+        </main>
       </div>
       
     </div>
